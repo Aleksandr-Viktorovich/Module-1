afd80d19786d6bc895421cf7f9825257fc676d60
Aleksandr <teanvkz@gmail.com>
2022-10-19 21:03:42 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+const productName = 'television';
+const amountProduct = 10;
+const categotyProduct = 'technique';
+const priceProduct = 10_000;
+
+
+
+console.log(productName);
+
+console.log(amountProduct * priceProduct)
+
