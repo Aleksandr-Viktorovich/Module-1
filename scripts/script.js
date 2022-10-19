98060eeafb75b298105ed89fc386e927e839327a
Aleksandr <teanvkz@gmail.com>
2022-10-19 21:45:05 +0300

    сообщение

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
 'use strict'
 
+{
+    const productName = 'television';
+    const amountProduct = 10;
+    const categotyProduct = 'technique';
+    const priceProduct = 10_000;
 
-const productName = 'television';
-const amountProduct = 10;
-const categotyProduct = 'technique';
-const priceProduct = 10_000;
 
 
+    console.log(productName);
 
-console.log(productName);
+    console.log(amountProduct * priceProduct);
+}
 
-console.log(amountProduct * priceProduct)
 
+{
+    const productName = 'camera';
+    const categotyProduct = 'photo';
+
+    console.log (productName)
+}
